d31e26dea853c8aa1224e6fa0ced8766aae124e7
EbenEzer-MOMBO <ebenezermombo@gmail.com>
2024-02-27 23:48:01 +0100

    mise a zero

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ebenezer-mombo.github.io',
-    base: '/portfolio',
 });
