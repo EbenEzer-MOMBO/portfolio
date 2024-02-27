1267bcc29950d9c04d332f22d51472ec683fd24a
EbenEzer-MOMBO <ebenezermombo@gmail.com>
2024-02-27 22:24:13 +0100

    css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/EbenEzer-MOMBO/portfolio.github.io',
-    base: '/',
+    site: 'https://github.com/EbenEzer-MOMBO/portfolio',
+    base: '/portfolio',
 });
